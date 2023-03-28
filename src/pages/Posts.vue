9a53840bf85aca9af6c6a68f4c07d01bde9c2ef4
ShammasAmir <amirshammas@gmail.com>
2023-03-28 14:02:34 +0330

    Create users page and posts page.Install and config axios.

---
+++ src/pages/Posts.vue
@@ -0,0 +1,29 @@
+<template>
+    <h1>
+        This is posts page !
+    </h1>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    setup() {
+        function getPosts() {
+            axios.get('https://jsonplaceholder.typicode.com/posts')
+                .then(function (response) {
+                    // handle success
+                    console.log(response.data);
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                });
+        }
+        getPosts()
+    }
+}
+</script>
+
+<style>
+    
+</style>
---
+++ src/pages/Users.vue
@@ -0,0 +1,27 @@
+<template>
+    <h1>
+        This is users page !
+    </h1>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    setup() {
+        function getUsers() {
+            axios.get('https://jsonplaceholder.typicode.com/users')
+                .then(function (response) {
+                    // handle success
+                    console.log(response.data);
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                });
+        }
+        getUsers()
+    }
+}
+</script>
+
+<style></style>
