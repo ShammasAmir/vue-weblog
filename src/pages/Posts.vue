acd88776d15fd5cc6a9937577112e4c42d163360
ShammasAmir <amirshammas@gmail.com>
2023-03-28 20:50:33 +0330

    Add loading spinner to users page and posts page.

--- src/pages/Posts.vue
+++ src/pages/Posts.vue
@@ -1,8 +1,11 @@
 <template>
     <div class="container mt-5">
         <div class="row g-3">
-            <div class="col-md-4" v-for="post in posts" :key="post.id">
-                <CardPost  :post="post" />
+            <div v-if="loading" class="spinner-border text-danger" role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
+            <div v-else class="col-md-4" v-for="post in posts" :key="post.id">
+                <CardPost :post="post" />
             </div>
         </div>     
     </div>
@@ -18,11 +21,13 @@ export default {
     },
     setup() {
         const posts = ref([])
+        const loading = ref(true)
         function getPosts() {
             axios.get('https://jsonplaceholder.typicode.com/posts')
                 .then(function (response) {
                     // handle success
                     posts.value = response.data;
+                    loading.value = false
                 })
                 .catch(function (error) {
                     // handle error
--- src/pages/Users.vue
+++ src/pages/Users.vue
@@ -1,8 +1,11 @@
 <template>
     <div class="container mt-5">
         <div class="row g-3">
-            <div class="col-md-4" v-for="user in users" :key="user.id">
-                <CardUser  :user="user" />
+            <div v-if="loading" class="spinner-border text-warning" role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
+            <div v-else class="col-md-4" v-for="user in users" :key="user.id">
+                <CardUser :user="user" />
             </div>
         </div>     
     </div>
@@ -18,11 +21,13 @@ export default {
     },
     setup() {
         const users = ref([])
+        const loading = ref(true)
         function getUsers() {
             axios.get('https://jsonplaceholder.typicode.com/users')
                 .then(function (response) {
                     // handle success
                     users.value = response.data;
+                    loading.value = false
                 })
                 .catch(function (error) {
                     // handle error
